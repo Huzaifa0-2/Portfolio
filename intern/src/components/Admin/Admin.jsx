0597a5a9d6d201c298273e3186b3f26e6034a8e2
Huzaifa0-2 <perfect.huzaifa01@gmail.com>
2025-04-19 00:14:15 +0500

    Project form

--- intern/src/components/Admin/Admin.jsx
+++ intern/src/components/Admin/Admin.jsx
@@ -3,15 +3,55 @@ import React from 'react';
 const Admin = () => {
   return (
     <div>
-      <div>
-        <h1>Admin Page</h1>
-        <div>
-          <form>
-            
-          </form>
+            <div className="min-h-screen bg-black">
+                <div className="flex items-center justify-center p-4">
+                    <form  className="w-full max-w-md bg-neutral-950 rounded-xl shadow-md border-2 border-orange-600 overflow-hidden p-8 mt-30">
+                        <h2 className="text-2xl font-bold text-center text-white mb-6"><span className='text-orange-600'>Add </span>Project</h2>
+
+                        <div className="mb-6">
+                            <label htmlFor="title" className="block text-sm font-medium text-white mb-1">
+                                Title
+                            </label>
+                            <input
+                                required
+                                type="text"
+                                id="title"
+                                className="w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
+                            />
+                        </div>
+
+                        <div className="mb-8">
+                            <label htmlFor="githublink" className="block text-sm font-medium text-white mb-1">
+                                Drop Project Link
+                            </label>
+                            <input
+                                type="text"
+                                id="githublink"
+                                className="w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
+                            />
+                        </div>
+
+                        <div className="mb-8">
+                            <label htmlFor="description" className="block text-sm font-medium text-white mb-1">
+                                Description
+                            </label>
+                            <textarea   
+                                type="text"
+                                id="description"
+                                className="w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full hover:bg-orange-600 text-white font-semibold py-3 px-4 rounded-lg border border-orange-600 shadow-md shadow-orange-600 transition duration-200 hover:shadow-none"
+                        >
+                            Add to Portfolio
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
   );
 };
 
--- intern/src/components/Admin/Login/Login.jsx
+++ intern/src/components/Admin/Login/Login.jsx
@@ -14,8 +14,8 @@ const Login = () => {
         e.preventDefault();
         try {
             // Get input values from your form
-              const email = e.target.email.value; 
-              const password = e.target.password.value;
+            const email = e.target.email.value;
+            const password = e.target.password.value;
 
             // Query Firestore for matching document
 
@@ -29,10 +29,10 @@ const Login = () => {
             let userData = docsRef.docs[0].data();
             console.log(userData)
 
-              if (userData.email === email && userData.password === password) {
+            if (userData.email === email && userData.password === password) {
                 console.log("Login successful");
                 navigate("/admin");
-              }
+            }
 
         } catch (e) {
             console.error("Error checking credentials: ", e);
@@ -49,46 +49,48 @@ const Login = () => {
     // };
 
     return (
-        <div> <div className="min-h-screen bg-black">
-            <div className="flex items-center justify-center p-4">
-                <form onSubmit={getData} className="w-full max-w-md bg-neutral-950 rounded-xl shadow-md border-2 border-orange-600 overflow-hidden p-8 mt-30">
-                    <h2 className="text-2xl font-bold text-center text-white mb-6"><span className='text-orange-600'>A</span>dmin Login</h2>
+        <div>
+            <div className="min-h-screen bg-black">
+                <div className="flex items-center justify-center p-4">
+                    <form onSubmit={getData} className="w-full max-w-md bg-neutral-950 rounded-xl shadow-md border-2 border-orange-600 overflow-hidden p-8 mt-30">
+                        <h2 className="text-2xl font-bold text-center text-white mb-6"><span className='text-orange-600'>A</span>dmin Login</h2>
 
-                    <div className="mb-6">
-                        <label htmlFor="email" className="block text-sm font-medium text-white mb-1">
-                            Username
-                        </label>
-                        <input
-                            required
-                            type="text"
-                            id="email"
-                            placeholder="Enter admin username"
-                            className="placeholder:text-gray-700 w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
-                        />
-                    </div>
+                        <div className="mb-6">
+                            <label htmlFor="email" className="block text-sm font-medium text-white mb-1">
+                                Username
+                            </label>
+                            <input
+                                required
+                                type="text"
+                                id="email"
+                                placeholder="Enter admin username"
+                                className="placeholder:text-gray-700 w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
+                            />
+                        </div>
 
-                    <div className="mb-8">
-                        <label htmlFor="password" className="block text-sm font-medium text-white mb-1">
-                            Password
-                        </label>
-                        <input
-                            required
-                            type="password"
-                            id="password"
-                            placeholder="Enter your password"
-                            className="placeholder:text-gray-700 w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
-                        />
-                    </div>
+                        <div className="mb-8">
+                            <label htmlFor="password" className="block text-sm font-medium text-white mb-1">
+                                Password
+                            </label>
+                            <input
+                                required
+                                type="password"
+                                id="password"
+                                placeholder="Enter your password"
+                                className="placeholder:text-gray-700 w-full px-4 py-3 bg-white rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent transition-all"
+                            />
+                        </div>
 
-                    <button
-                        type="submit"
-                        className="w-full hover:bg-orange-600 text-white font-semibold py-3 px-4 rounded-lg border border-orange-600 shadow-md shadow-orange-600 transition duration-200 hover:shadow-none"
-                    >
-                        Sign In
-                    </button>
-                </form>
+                        <button
+                            type="submit"
+                            className="w-full hover:bg-orange-600 text-white font-semibold py-3 px-4 rounded-lg border border-orange-600 shadow-md shadow-orange-600 transition duration-200 hover:shadow-none"
+                        >
+                            Sign In
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div></div>
+        </div>
     )
 }
 
